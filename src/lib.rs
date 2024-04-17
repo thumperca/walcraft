aa41ed1955f75118c2a078b927890c7b4245ab84
Manvir Singh <mail@manvir.me>
2024-04-17 22:34:50 +0530

    Added missing files from git

---
+++ src/lib.rs
@@ -0,0 +1,83 @@
+// Wal > read
+// Wal > write > buffer > file
+
+// Wal -> Reader
+// Wal -> Writer
+// Wal -> Writer -> Buffer
+// Wal -> Writer -> FileManager
+// Wal -> Writer -> FileManager -> Queue[Buffer]
+// Wal -> Writer -> FileManager -> FileHandle
+
+mod writer;
+use self::writer::Writer;
+use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
+use std::sync::atomic::AtomicU8;
+use std::sync::Arc;
+
+const MODE_IDLE: u8 = 0;
+const MODE_READ: u8 = 1;
+const MODE_WRITE: u8 = 2;
+
+struct WalInner<T>
+where
+    T: Serialize + for<'a> Deserialize<'a>,
+{
+    mode: AtomicU8,
+    writer: Writer,
+    _phantom: PhantomData<T>,
+}
+
+impl<T> WalInner<T>
+where
+    T: Serialize + for<'a> Deserialize<'a>,
+{
+    pub fn new(location: &str, size: Option<usize>) -> Self {
+        Self {
+            mode: AtomicU8::new(MODE_IDLE),
+            writer: Writer::new(location, size),
+            _phantom: PhantomData,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Wal<T>
+where
+    T: Serialize + for<'a> Deserialize<'a>,
+{
+    inner: Arc<WalInner<T>>,
+}
+
+impl<T> Wal<T>
+where
+    T: Serialize + for<'a> Deserialize<'a>,
+{
+    /// Create a new instance of [Wal]
+    /// # Arguments
+    /// - location: Location where the files shall be stored
+    /// - size: Optional, maximum storage size taken by logs
+    pub fn new(location: &str, size: Option<u16>) -> Self {
+        todo!()
+    }
+
+    /// Read last `num` amount of logs
+    pub fn read(&self, num: usize) -> Vec<T> {
+        todo!()
+    }
+
+    /// Read all stored logs
+    pub fn read_all(&self) -> Vec<T> {
+        todo!()
+    }
+
+    /// Write a new log
+    pub fn write(&self, item: T) {
+        todo!()
+    }
+
+    /// Delete all the stored logs
+    pub fn clear(&self) {
+        todo!()
+    }
+}
