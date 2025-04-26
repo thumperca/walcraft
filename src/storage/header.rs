68b070e8cfb8538f1fdc73fdc6df399549e1da0c
Manvir Singh <mail@manvir.me>
2025-04-26 04:37:48 +0000

    Refactoring storage module

---
+++ src/storage/header.rs
@@ -0,0 +1,64 @@
+/// Header for heap file that stores metadata on the file
+///
+/// This metadata is essential when reading the file back
+/// as the version or page_size may changes over time
+pub(crate) struct Header {
+    version: usize,
+    page_size: usize,
+    num_pages: usize,
+}
+
+impl Header {
+    /// Creates a new header with the given version, page size and number of pages
+    fn new(version: usize, page_size: usize) -> Self {
+        Header {
+            version,
+            page_size,
+            num_pages: 0,
+        }
+    }
+
+    /// Serializes the header to a byte array
+    fn as_bytes(&self) -> [u8; 4096] {
+        let mut data = [0; 4096];
+
+        // write the header signature
+        data[0..4].copy_from_slice("HEAD".as_bytes());
+
+        // write the version, page size and number of pages
+        data[4..12].copy_from_slice(&self.version.to_le_bytes());
+        data[12..20].copy_from_slice(&self.page_size.to_le_bytes());
+        data[20..28].copy_from_slice(&self.num_pages.to_le_bytes());
+
+        data
+    }
+}
+
+/// Create a new header from a byte array
+impl TryFrom<&[u8]> for Header {
+    type Error = String;
+
+    fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
+        // header shall be at least 28 bytes long
+        if data.len() < 28 {
+            return Err("Header length is too short for serialization".to_string());
+        }
+
+        // ensure the first 4 bytes are "HEAD"
+        let sign = &data[0..4];
+        if sign != "HEAD".as_bytes() {
+            return Err(format!("Invalid header signature: {:?}", sign));
+        }
+
+        // read the version, page size and number of pages
+        let version = usize::from_le_bytes(data[4..12].try_into().unwrap());
+        let page_size = usize::from_le_bytes(data[12..20].try_into().unwrap());
+        let num_pages = usize::from_le_bytes(data[20..28].try_into().unwrap());
+
+        Ok(Header {
+            version,
+            page_size,
+            num_pages,
+        })
+    }
+}
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,68 +1,7 @@
+mod header;
+
+use self::header::Header;
 use crate::WalConfig;
-
-/// Header for heap file that stores metadata on the file
-///
-/// This metadata is essential when reading the file back
-/// as the version or page_size may changes over time
-struct Header {
-    version: usize,
-    page_size: usize,
-    num_pages: usize,
-}
-
-impl Header {
-    /// Creates a new header with the given version, page size and number of pages
-    fn new(version: usize, page_size: usize) -> Self {
-        Header {
-            version,
-            page_size,
-            num_pages: 0,
-        }
-    }
-
-    /// Serializes the header to a byte vector
-    fn as_bytes(&self) -> [u8; 4096] {
-        let mut data = [0; 4096];
-
-        // write the header signature
-        data[0..4].copy_from_slice("HEAD".as_bytes());
-
-        // write the version, page size and number of pages
-        data[4..12].copy_from_slice(&self.version.to_le_bytes());
-        data[12..20].copy_from_slice(&self.page_size.to_le_bytes());
-        data[20..28].copy_from_slice(&self.num_pages.to_le_bytes());
-
-        data
-    }
-}
-
-impl TryFrom<&[u8]> for Header {
-    type Error = String;
-
-    fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
-        // header shall be at least 28 bytes long
-        if data.len() < 28 {
-            return Err("Header length is too short for serialization".to_string());
-        }
-
-        // ensure the first 4 bytes are "HEAD"
-        let sign = &data[0..4];
-        if sign != "HEAD".as_bytes() {
-            return Err(format!("Invalid header signature: {:?}", sign));
-        }
-
-        // read the version, page size and number of pages
-        let version = usize::from_le_bytes(data[4..12].try_into().unwrap());
-        let page_size = usize::from_le_bytes(data[12..20].try_into().unwrap());
-        let num_pages = usize::from_le_bytes(data[20..28].try_into().unwrap());
-
-        Ok(Header {
-            version,
-            page_size,
-            num_pages,
-        })
-    }
-}
 
 struct File {
     header: Header,
